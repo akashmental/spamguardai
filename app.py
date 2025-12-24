97952677af1e526331bfe10cfc6631da00add855
Akash <apv5477@gmail.com>
2025-12-24 18:34:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+import model  # ML model file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/classifier')
+def classifier():
+    return render_template('classifier.html')
+
+@app.route('/dataset')
+def dataset():
+    return render_template('dataset.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.is_json:
+        data = request.get_json()
+        user_message = data.get("message", "")
+        prediction = model.predict(user_message)
+        return jsonify({"prediction": prediction})
+    else:
+        # fallback for normal form submission
+        user_message = request.form['message']
+        prediction = model.predict(user_message)
+        return render_template('classifier.html', prediction=prediction, user_message=user_message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
