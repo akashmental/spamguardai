256cb42a8eaa67ec34654a41c305d8b64d40e167
Akash <apv5477@gmail.com>
2026-01-05 23:08:14 +0530

    Fix merge conflict in model.py

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -51,7 +51,7 @@ print(f"Accuracy : {accuracy:.2f}%")
 print(f"Precision: {precision:.2f}%")
 print(f"Recall   : {recall:.2f}%")
 print(f"F1-Score : {f1:.2f}%")
-=======
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -104,5 +104,4 @@ print(f"Accuracy : {accuracy:.2f}%")
 print(f"Precision: {precision:.2f}%")
 print(f"Recall   : {recall:.2f}%")
 print(f"F1-Score : {f1:.2f}%")
->>>>>>> 24f5cb8 (Initial commit: Flask spam classifier app)
 print("========================================")
